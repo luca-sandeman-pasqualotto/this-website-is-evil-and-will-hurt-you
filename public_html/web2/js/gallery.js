f20a8ecd84445b90f51a0c101740e70b7217947d
Luca <6297451@edu.vaniercollege.qc.ca>
2026-02-05 12:04:29 -0500

    woaw look at these cool pictures

--- public_html/web2/js/gallery.js
+++ public_html/web2/js/gallery.js
@@ -1,17 +1,22 @@
-console.log('Gallery!!! BOO!')
+console.log('Gallery!!! BOO!');
 
-const thumbnails = document.querySelectorAll('figure')
-console.info(thumbnails)
+const thumbnails = document.querySelectorAll('figure');
+console.info(thumbnails);
 
-const modal = document.querySelector('dialog')
+const modal = document.querySelector('dialog');
 
 thumbnails.forEach(thumb => {
-    console.log(thumb)
-    thumb.addEventListener('click', loadModal)
+    console.log(thumb);
+    thumb.addEventListener('click', loadModal);
 })
 
 function loadModal(event) {
-    console.info(event.target.closest('figure').innerHTML)
-    modal.querySelector('figure').innerHTML = (event.target.closest('figure').innerHTML)
-    modal.showModal()
+    console.info(event.target.closest('figure').querySelector('img').src);
+    let thumbImg = event.target.closest('figure').querySelector('img').src;
+    thumbImg = thumbImg.substring(thumbImg.lastIndexOf('/'));
+    console.info(thumbImg);
+
+    modal.querySelector('figure').innerHTML = (event.target.closest('figure').innerHTML);
+    modal.querySelector('img').src = 'photos' + thumbImg;
+    modal.showModal();
 }
