2549581651c61177b6638f4fb1cb27a32208a233
Nick <luca.sandeman.pasqualotto@gmail.com>
2026-09-13 20:11:13 -0400

    erm cool project

--- public_html/web3/tictactoe/js/ttt.js
+++ public_html/web3/tictactoe/js/ttt.js
@@ -1,14 +1,15 @@
-// console.log("loaded!")
+console.log("Loaded!")
 
-// // references
- let boxes = document.querySelectorAll(".box");
+// RÃ©fÃ©rences HTML
+let cases = document.querySelectorAll(".case");
 let replayBtn = document.querySelector("#replay");
- let panneauMessage = document.querySelector("#message");
+let panneauMessage = document.querySelector("#message");
+let panneauMessageGagnant = document.querySelector("#message img");
 
-// // variables
-let PlayerOne = true; //first player is X
-let winner = ''; //no winner yet
-const conditions = [ //these are the winning numbers
+// Variables de lâ€™app
+let joueurX = true; //premier joueur X
+let gagnant = ''; //pas encore de gagnant
+const patrons = [ //les patrons gagnants
     [0, 1, 2],
     [0, 3, 6],
     [0, 4, 8],
@@ -19,67 +20,140 @@ const conditions = [ //these are the winning numbers
     [6, 7, 8]
 ];
 
-// // clicking boxes
-for (let square of boxes) {
-    square.active = true;
-    square.addEventListener("click", function () {
-       
-        // this randomly chooses a sprite
-        let sprite = Math.floor(Math.random() * 2);
-        let spriteMeasure = "-" + sprite + "00%"
-        console.log(spriteMeasure)
-        if (square.active) {
-            if (PlayerOne) { //tour du joueur X
-                square.style.backgroundImage = "url('./img/n_sprite.svg')";
-                square.style.backgroundPosition = spriteMeasure;
-                PlayerOne = false;
+// function getImageURL(bg) {
+//     return bg.slice(-7, -2);
+// }
+
+document.body.classList.add("player-n")
+document.getElementsByClassName("left_player")[0].classList.add("player-n")
+// Fonctions
+
+for (let boite of cases) {
+    boite.active = true;
+    boite.addEventListener("click", function () {
+        
+        // math for sprites
+        var nimg = ["./n_img/1n.svg", "./n_img/2n.svg"]
+        var randomImgN = nimg [Math.floor(Math.random() * nimg.length)];
+        console.log(randomImgN)
+
+        var simg = ["./s_img/1s.svg", "./s_img/2s.svg"]
+        var randomImgS = simg [Math.floor(Math.random() * simg.length)];
+        
+        if (boite.active) {
+            if (joueurX) { //tour du joueur X
+                //boite.innerText = "X";
+                boite.style.backgroundImage = `url('${randomImgN}')`;
+                joueurX = false;
             }
             else { //tour du jour O
-                square.style.backgroundImage = "url('./img/s_sprite.svg')";
-                square.style.backgroundPosition = spriteMeasure;
-                PlayerOne = true;
+                //boite.innerText = "O";
+                boite.style.backgroundImage = `url('${randomImgS}')`;
+                joueurX = true;
             }
-            square.style.backgroundSize = "cover"
-            document.body.classList.toggle("player-0");
-            document.body.classList.toggle("player-1");
-            square.active = false;
-            valid();
+            boite.active = false;
+            document.body.classList.toggle("player-n")[0];
+            document.body.classList.toggle("player-s")[0];
+            valide();
         }
      });
 }
 
-// valid: who wins or loses
-const valid = function () {
-    if ([...boxes].every((square) => square.active === false)) {
-        //no boxes available
-        afficheMessage("partie nulle");
+// Valide lâ€™Ã©tat du jeu: gagnant ou nulle
+const valide = function () {
+    if ([...cases].every((boite)  => boite.active === false)) { //toutes les cases sont inactives
+        showWinnerText("Partie nul");
     }
-    else { //if there are no boxes but someone won
-        for (let condition of conditions) { //winning patterns
-            let val1 = boxes[condition[0]].style.backgroundImage.slice(5, 17);;
-            let val2 = boxes[condition[1]].innerText;
-            let val3 = boxes[condition[2]].innerText;
-            // the values of the winning patterns
+    else { //sinon valide gagnant
+        for (let patron of patrons) { //boucle des patrons gagnants
+            let val1 = cases[patron[0]].style.backgroundImage.slice(5,10); //les valeurs des positions du patron
+            let val2 = cases[patron[1]].style.backgroundImage.slice(5,10);
+            let val3 = cases[patron[2]].style.backgroundImage.slice(5,10);
+            // const val1 = getImageURL(cases[0].style.backgroundImage);
+            // const val2 = getImageURL(cases[1].style.backgroundImage);
+            // const val3 = getImageURL(cases[2].style.backgroundImage);
+
             if (val1 &&
                 val1 === val2 &&
-                val1 === val3) { //
-                afficheMessage(`Le gagnant est ${val1}`);
-                for (let square of boxes) {
-                    square.active = false;
+                val1 === val3) {  //nous avons on gagnant
+                // victoire = true;
+
+                // let winner = "";
+                // if (val1.includes("n.svg")) {
+                //     winner = "x"
+                //     afficheMessage("Le gagnant est Nintendo!")
+                // }
+                // else {
+                //     winner = "o"
+                //     afficheMessage("Le gagnant est SEGA!")
+                // }
+
+                //afficheMessage(`Le gagnant est ${val1}`);
+                showWinnerText(`Le gagnant est ${val1}!`);
+                    // afficheMessage(`Le gagnant est ${val1}!`);
+                    for (let boite of cases) {
+                        boite.active = false;
                 }
             }
         }
     }
 };
 
-// erase the boxes
-const eraseBoxes = function () {
-    for (let square of boxes) {
-        square.innerText = '';
+//Affiche message
+const afficheMessage = function (msg) {
+        panneauMessage.innerText = msg;
+};
+
+// changing text color on player turn
+document.getElementById("grille").onclick=function(){
+    const pTurn = document.getElementById("players");
+    const nPlayer = document.getElementsByClassName("left_player")[0];
+    const sPlayer = document.getElementsByClassName("right_player")[0];
+    nPlayer.classList.toggle("player-n");
+    sPlayer.classList.toggle("player-s");
+    
+}
+
+// console.log(tColor.style.color)
+
+//Vide les cases
+const videCases = function () {
+    for (let boite of cases) {
+        boite.innerText = '';
+        boite.style.backgroundImage = "";
     }
 };
 
-// affiche message
-const afficheMessage = function (msg) {
-    panneauMessage.innerText = msg;
-};
+
+//Jouer encore
+const nPlayer = document.getElementsByClassName("left_player")[0];
+const sPlayer = document.getElementsByClassName("right_player")[0];
+replayBtn.addEventListener("click", function() {
+    videCases();
+    afficheMessage("");
+    joueurX = true;
+    for (let boite of cases) {
+        boite.active = true;
+    }
+    
+    // resets players
+    document.body.classList.remove("player-s")
+    document.body.classList.add("player-n")
+    nPlayer.classList.add("player-n")
+    sPlayer.classList.remove("player-s")
+
+    hideWinner();
+});
+
+
+const winner = document.getElementById("winner");
+const winnerMessage = document.getElementById("message");
+
+function showWinnerText(text) {
+    winnerMessage.textContent = text;
+    winner.classList.remove("hidden")
+}
+
+function hideWinner() {
+    winner.classList.add("hidden")
+}
